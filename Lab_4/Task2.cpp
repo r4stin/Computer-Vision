85a58a6e0e6f63754ec4157fe1a414a1d5774016
Rastin <r.akbari625@gmail.com>
2024-04-11 19:43:41 +0200

    Task2 Updated

--- Lab_4/Task2.cpp
+++ Lab_4/Task2.cpp
@@ -3,9 +3,15 @@
 
 
 
-int main() {
+int main(int argc, char** argv ) {
+
+    if (argc < 2) {
+        std::cout << "Image filename shall be provided!" << std::endl;
+        return -1;
+
+    }
     // Load the image
-    cv::Mat image = cv::imread("street_scene.png");
+    cv::Mat image =  imread(argv[1], cv::IMREAD_COLOR);
     if (image.empty()) {
         std::cerr << "Error: Unable to load image!" << std::endl;
         return -1;
@@ -14,7 +20,7 @@ int main() {
     // Detect the markings
     cv::Mat overlay = detectMarkings(image);
     // Show the result
-    ShowImage(overlay, "Edges on Original Image");
+    ShowImage(overlay, "Detected Markings");
 
     // Wait for a key press
     cv::waitKey(0);
