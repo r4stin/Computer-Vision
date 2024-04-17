b24912d7d8fd8f867fc139f28425e018505bf5df
Rastin <r.akbari625@gmail.com>
2024-04-17 15:19:54 +0200

    Task2 completed

---
+++ Lab_5/Task2.cpp
@@ -0,0 +1,31 @@
+#include "header.h"
+#include "functions.h"
+
+
+
+int main(int argc, char** argv ) {
+
+    if (argc < 2) {
+        std::cout << "Image filename shall be provided!" << std::endl;
+        return -1;
+
+    }
+    // Load the image
+    cv::Mat image =  imread(argv[1], cv::IMREAD_COLOR);
+    if (image.empty()) {
+        std::cerr << "Error: Unable to load image!" << std::endl;
+        return -1;
+    }
+
+    cv::Mat app1 = task2App1(image);
+    ShowImage(app1, "App1 Result");
+    cv::Mat app2 = KMeans(image, 3);
+    ShowImage(app2, "App2 Result");
+
+
+    // Wait for a key press
+    cv::waitKey(0);
+    return 0;
+}
+
+
